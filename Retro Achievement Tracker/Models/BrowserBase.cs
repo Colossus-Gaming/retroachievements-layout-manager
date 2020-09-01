88cc2c9b8363749742bdb08157c228751826a578
lrhodes404 <lrhodes404@gmail.com>
2020-09-01 12:43:49 -0400

    Validated Mastery was still functional

---
+++ Retro Achievement Tracker/Models/BrowserBase.cs
@@ -0,0 +1,38 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retro_Achievement_Tracker.Models
+{
+    public class BrowserBase
+    {
+        public CefSharp.WinForms.ChromiumWebBrowser WebBrowser { get; }
+        private CefSharp.WinForms.ChromiumWebBrowser _chromiumWebBrowser;
+
+        public BrowserBase()
+        {
+            SetupBrowser();
+        }
+
+        private void SetupBrowser()
+        {
+            this._chromiumWebBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            // 
+            // chromiumWebBrowser
+            // 
+            this._chromiumWebBrowser.ActivateBrowserOnCreation = false;
+            this._chromiumWebBrowser.Location = new System.Drawing.Point(240, 0);
+            this._chromiumWebBrowser.Name = "chromiumWebBrowser";
+            this._chromiumWebBrowser.Size = new System.Drawing.Size(1920, 1080);
+            this._chromiumWebBrowser.TabIndex = 1;
+            this._chromiumWebBrowser.Dock = DockStyle.None;
+            this._chromiumWebBrowser.RequestHandler = new CustomRequestHandler();
+        }
+
+
+    }
+}
