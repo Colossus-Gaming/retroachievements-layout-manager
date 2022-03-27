44d4695a8b369b6bdac51aafd0c734502efafb77
Lamar Rhodes <lrhodes404@gmail.com>
2022-03-26 23:40:18 -0400

    Setting background color on window launch for achievement list

---
+++ Retro Achievement Tracker/Controllers/AchievementListController.cs
@@ -0,0 +1,276 @@
+﻿using Retro_Achievement_Tracker.Forms;
+using Retro_Achievement_Tracker.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retro_Achievement_Tracker.Controllers
+{
+    public sealed class AchievementListController
+    {
+        private static AchievementListController instance = new AchievementListController();
+        private static AchievementListWindow AchievementListWindow;
+        public static bool IsOpen;
+        private List<Achievement> CurrentUnlockedAchievements;
+        private List<Achievement> CurrentLockedAchievements;
+        private bool IsAnimating;
+
+        private AchievementListController()
+        {
+            AchievementListWindow = new AchievementListWindow();
+
+            CurrentUnlockedAchievements = new List<Achievement>();
+            CurrentLockedAchievements = new List<Achievement>();
+
+            IsOpen = false;
+        }
+        public static AchievementListController Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public void Close()
+        {
+            AchievementListWindow.Close();
+        }
+        public void Show()
+        {
+            if (!IsOpen)
+            {
+                AchievementListWindow = new AchievementListWindow();
+
+                CurrentUnlockedAchievements = new List<Achievement>();
+                CurrentLockedAchievements = new List<Achievement>();
+
+                AchievementListWindow.Show();
+            }
+        }
+
+        public void SetAllSettings()
+        {
+
+        }
+
+        public async void UpdateAchievementList(List<Achievement> unlockedAchievements, List<Achievement> lockedAchievements, bool newGame)
+        {
+
+            if (IsOpen)
+            {
+                if (newGame)
+                {
+                    List<Achievement> ToClearList = new List<Achievement>();
+                    ToClearList.AddRange(CurrentUnlockedAchievements);
+                    ToClearList.AddRange(CurrentLockedAchievements);
+
+                    CurrentUnlockedAchievements = new List<Achievement>();
+                    CurrentLockedAchievements = new List<Achievement>();
+
+                    Random rand = new Random();
+
+                    while (ToClearList.Count > 0)
+                    {
+                        Achievement achievement = ToClearList[rand.Next(ToClearList.Count - 1)];
+                        AchievementListWindow.ClearAchievement(achievement.Id);
+
+                        ToClearList.RemoveAt(ToClearList.IndexOf(achievement));
+
+                        await Task.Delay(20);
+                    }
+
+                    await Task.Delay(500);
+
+                    AchievementListWindow.ClearList();
+                }
+
+                for (int i = 0; i < unlockedAchievements.Count; i++)
+                {
+                    Achievement newAchievement = (Achievement)unlockedAchievements[i].Clone();
+                    Achievement oldAchievement = CurrentLockedAchievements.Find(achievement1 => achievement1.Id == newAchievement.Id);
+
+                    if (CurrentUnlockedAchievements.Find(achievement1 => achievement1.Id == newAchievement.Id) == null)
+                    {
+                        CurrentUnlockedAchievements.Add(newAchievement);
+
+                        if (oldAchievement == null)
+                        {
+                            AchievementListWindow.AddAchievement(newAchievement, GetImageSize(unlockedAchievements.Count + lockedAchievements.Count));
+                        }
+                    }
+                    else
+                    {
+                        AchievementListWindow.UnlockAchievement(newAchievement);
+                    }
+
+                    if (oldAchievement != null)
+                    {
+                        CurrentLockedAchievements.Remove(oldAchievement);
+                    }
+                }
+
+                for (int i = 0; i < lockedAchievements.Count; i++)
+                {
+                    if (CurrentLockedAchievements.Find(achievement1 => achievement1.Id == lockedAchievements[i].Id) == null)
+                    {
+                        Achievement newAchievement = (Achievement)lockedAchievements[i].Clone();
+
+                        CurrentLockedAchievements.Add(newAchievement);
+
+                        AchievementListWindow.AddAchievement(newAchievement, GetImageSize(unlockedAchievements.Count + lockedAchievements.Count));
+                    }
+                }
+
+                if (CurrentUnlockedAchievements.Count > 0 || CurrentLockedAchievements.Count > 0)
+                {
+                    AnimateAchievementList();
+                }
+            }
+        }
+        public async void AnimateAchievementList()
+        {
+            if (IsOpen && !IsAnimating)
+            {
+                IsAnimating = true;
+
+                CurrentUnlockedAchievements.Sort();
+                CurrentUnlockedAchievements.Reverse();
+
+                for (int i = 0; i < CurrentUnlockedAchievements.Count; i++)
+                {
+                    AchievementListWindow.UnlockAchievement(CurrentUnlockedAchievements[i]);
+                    AchievementListWindow.SetAchievementPosition(CurrentUnlockedAchievements[i].Id, GetAchievementLocationX(i), GetAchievementLocationY(i));
+                    await Task.Delay(30);
+                }
+
+                CurrentLockedAchievements.Sort();
+
+                for (int i = 0; i < CurrentLockedAchievements.Count; i++)
+                {
+                    AchievementListWindow.SetAchievementPosition(CurrentLockedAchievements[i].Id, GetAchievementLocationX(i + CurrentUnlockedAchievements.Count), GetAchievementLocationY(i + CurrentUnlockedAchievements.Count));
+                    await Task.Delay(30);
+                }
+
+                IsAnimating = false;
+            }
+        }
+        private int GetAchievementLocationX(int achievementIndex)
+        {
+            return 10 + (achievementIndex % Convert.ToInt32(Math.Sqrt(CurrentUnlockedAchievements.Count + CurrentLockedAchievements.Count))) * GetImageSize(CurrentUnlockedAchievements.Count + CurrentLockedAchievements.Count);
+        }
+        private int GetAchievementLocationY(int achievementIndex)
+        {
+            return 10 + (achievementIndex / Convert.ToInt32(Math.Sqrt(CurrentUnlockedAchievements.Count + CurrentLockedAchievements.Count))) * GetImageSize(CurrentUnlockedAchievements.Count + CurrentLockedAchievements.Count);
+        }
+        public bool AutoLaunch
+        {
+            get
+            {
+                return Settings.Default.auto_achievement_list;
+            }
+            set
+            {
+                Settings.Default.auto_achievement_list = value;
+                Settings.Default.Save();
+            }
+        }
+        public string BorderBackgroundColor
+        {
+            get
+            {
+                return Settings.Default.achievement_list_border_color;
+            }
+            set
+            {
+                Settings.Default.achievement_list_border_color = value;
+                Settings.Default.Save();
+
+                if (IsOpen)
+                {
+                    AchievementListWindow.SetBorderBackgroundColor(value);
+                }
+            }
+        }
+        public string WindowBackgroundColor
+        {
+            get
+            {
+                return Settings.Default.achievement_list_window_background_color;
+            }
+            set
+            {
+                Settings.Default.achievement_list_window_background_color = value;
+                Settings.Default.Save();
+                if (IsOpen)
+                {
+                    AchievementListWindow.SetWindowBackgroundColor(value);
+                }
+            }
+        }
+        public bool ColumnEnabled
+        {
+            get
+            {
+                return Settings.Default.achievement_list_column_enabled;
+            }
+            set
+            {
+                Settings.Default.achievement_list_column_enabled = value;
+                Settings.Default.Save();
+
+                if (IsOpen)
+                {
+                    AnimateAchievementList();
+                }
+            }
+        }
+        public int ContainerCount
+        {
+            get
+            {
+                return Settings.Default.achievement_list_container_count;
+            }
+            set
+            {
+                Settings.Default.achievement_list_container_count = value;
+                Settings.Default.Save();
+
+                if (IsOpen)
+                {
+                    AnimateAchievementList();
+                }
+            }
+        }
+        public bool OptimizeEnabled
+        {
+            get
+            {
+                return Settings.Default.achievement_list_optimize_enabled;
+            }
+            set
+            {
+                Settings.Default.achievement_list_optimize_enabled = value;
+                Settings.Default.Save();
+
+                if (IsOpen)
+                {
+                    AnimateAchievementList();
+                }
+            }
+        }
+        public int ColumnCount
+        {
+            get
+            {
+                return Convert.ToInt32(Math.Sqrt(CurrentUnlockedAchievements.Count + CurrentLockedAchievements.Count));
+            }
+        }
+        public int GetImageSize(int listSize)
+        {
+            return 760 / Convert.ToInt32(Math.Ceiling(Math.Sqrt(listSize)));
+        }
+    }
+}
---
+++ Retro Achievement Tracker/Forms/AchievementListWindow.cs
@@ -0,0 +1,176 @@
+﻿using CefSharp;
+using CefSharp.Web;
+using Retro_Achievement_Tracker.Controllers;
+using Retro_Achievement_Tracker.Models;
+using Retro_Achievement_Tracker.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Retro_Achievement_Tracker.Forms
+{
+    public partial class AchievementListWindow : Form
+    {
+        public AchievementListWindow()
+        {
+            ClientSize = new Size(0, 0);
+
+            Name = "RA Tracker - Achievement List";
+            Text = "RA Tracker - Achievement List";
+
+            Shown += AchievementListWindow_Shown;
+            FormClosed += AchievementListWindow_FormClosed;
+
+            SetupBrowser();
+        }
+
+        private void AchievementListWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            AchievementListController.IsOpen = false;
+        }
+
+        private void AchievementListWindow_Shown(object sender, EventArgs e)
+        {
+            AchievementListController.IsOpen = true;
+        }
+        public void AssignJavaScriptVariables()
+        {
+            ExecuteScript(
+                "allAchievements = document.getElementsByClassName(\"achievement\");");
+        }
+        public void AddAchievement(Achievement achievement, int size)
+        {
+            string badgeLink = achievement.DateEarned.HasValue ? "\"https://retroachievements.org/Badge/" + achievement.BadgeNumber + ".png\"" : "\"https://s3-eu-west-1.amazonaws.com/i.retroachievements.org/Badge/" + achievement.BadgeNumber + "_lock.png\"";
+            ExecuteScript("addAchievement(\"" + achievement.Title.Replace("\"", "\\\"") + "\"," +
+                                       "" + badgeLink + "," +
+                                       "\"" + achievement.Description.Replace("\"", "\\\"") + "\"," +
+                                       "\"" + achievement.Points + "\"," +
+                                       "\"" + achievement.DateEarned.ToString() + "\", " +
+                                       "\"" + achievement.Id + "\"," +
+                                       "\"" + size + "px\");");
+
+        }
+        public void SetBorderBackgroundColor(string value)
+        {
+            ExecuteScript(
+                "achievementElement.style.backgroundColor = \"" + value + "\";" +
+                "masteryElement.style.backgroundColor = \"" + value + "\";");
+        }
+        public void SetWindowBackgroundColor(string value)
+        {
+            ExecuteScript(
+                "container.style.backgroundColor = \"" + value + "\";");
+        }
+
+        public void UnlockAchievement(Achievement newAchievement)
+        {
+            ExecuteScript("document.getElementById(\"achievement-" + newAchievement.Id + "-image\").src = \"https://retroachievements.org/Badge/" + newAchievement.BadgeNumber + ".png\"");
+        }
+        public async Task<ValueTuple<int, int>> GetAchievementPosition(int id)
+        {
+            int currX = 0;
+            int currY = 0;
+
+            try
+            {
+                JavascriptResponse javascriptResponse = await chromiumWebBrowser.EvaluateScriptAsync("document.getElementById(\"achievement-" + id + "\").offsetLeft", TimeSpan.FromSeconds(5));
+
+                currX = int.Parse(javascriptResponse.Result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            try
+            {
+                JavascriptResponse javascriptResponse = await chromiumWebBrowser.EvaluateScriptAsync("document.getElementById(\"achievement-" + id + "\").offsetTop", TimeSpan.FromSeconds(5));
+
+                currY = int.Parse(javascriptResponse.Result.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return (currX, currY);
+        }
+        public async void SetAchievementPosition(int id, int nextX, int nextY)
+        {
+            await GetAchievementPosition(id).ContinueWith(result =>
+            {
+                ExecuteScript("document.getElementById(\"achievement-" + id + "\")" +
+                ".animate([ { left: '" + result.Result.Item1 + "px', top: '" + result.Result.Item2 + "px' }, { left: '" + nextX + "px', top: '" + nextY + "px' } ], { interations: 1, duration: 300, fill: \"forwards\", easing: \"ease-in\" });");
+            });
+        }
+        public async void ClearAchievement(int id)
+        {
+            await GetAchievementPosition(id).ContinueWith(result =>
+            {
+                ExecuteScript("document.getElementById(\"achievement-" + id + "\")" +
+                ".animate([ { left: '" + result.Result.Item1 + "px', top: '" + result.Result.Item2 + "px' }, { left: '" + result.Result.Item1 + "px', top: '1500px' } ], { interations: 1, duration: 500, fill: \"forwards\", easing: \"ease-in\" });" +
+                "document.getElementById(\"achievement-" + id + "\")" +
+                ".classList.add(\"achievement-delete\");");
+            });
+        }
+        public void ClearList()
+        {
+            ExecuteScript("var allDeleteAchievements = document.getElementsByClassName(\"achievement-delete\");" +
+                "while (allDeleteAchievements.length > 0) {  allDeleteAchievements[allDeleteAchievements.length - 1].remove(); allDeleteAchievements = document.getElementsByClassName(\"achievement-delete\"); }");
+        }
+        protected async void ExecuteScript(string script)
+        {
+            if (chromiumWebBrowser != null)
+            {
+                try
+                {
+                    await chromiumWebBrowser.EvaluateScriptAsync(script, TimeSpan.FromSeconds(5));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+        protected override bool ShowWithoutActivation
+        {
+            get { return true; }
+        }
+        public void SetupBrowser()
+        {
+            Controls.Remove(chromiumWebBrowser);
+
+            chromiumWebBrowser = new CefSharp.WinForms.ChromiumWebBrowser(new HtmlString(Resources.AchievementListWindow))
+            {
+                ActivateBrowserOnCreation = false,
+                Location = new Point(0, 0),
+                Name = "chromiumWebBrowser",
+                Size = new Size(778, 778),
+                TabIndex = 0,
+                Dock = DockStyle.None,
+                RequestHandler = new CustomRequestHandler()
+            };
+
+            chromiumWebBrowser.FrameLoadEnd += new EventHandler<FrameLoadEndEventArgs>((sender, frameLoadEndEventArgs) =>
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    ClientSize = new Size(768, 768);
+
+                    AchievementListController.Instance.SetAllSettings();
+                    SetWindowBackgroundColor(AchievementListController.Instance.WindowBackgroundColor);
+                });
+            });
+
+            Controls.Add(chromiumWebBrowser);
+        }
+
+        public CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser;
+    }
+}
